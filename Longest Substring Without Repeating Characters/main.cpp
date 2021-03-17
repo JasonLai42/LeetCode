6fca4c40565c32cb02c0d75534960efbb64d7275
Jason Lai <jasonlaiusa@yahoo.com>
2021-03-17 13:54:10 -0700

    slightly diff solution for longest substring

--- Longest Substring Without Repeating Characters/main.cpp
+++ Longest Substring Without Repeating Characters/main.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
     int lengthOfLongestSubstring(string s) {
         int final_max = 0;
@@ -40,5 +40,39 @@ public:
         }
         
         return final_max;
+    }
+};
+
+class Solution2 {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // Store the index at which this character was last seen in the string i.e. the highest index in s that this char was at
+        unordered_map<char, int> last_seen_index;
+        
+        // The length of the longest substring thus far
+        int longest = 0;
+        // Store the index in s where the substring we are currently computing starts
+        int first = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            auto temp = last_seen_index.find(s[i]);
+            // If we've seen this char before, "truncate" this char and everything before it in the last substring we were computing
+            // and set first as the index of the character after it (the new start of the substring we are currently computing)
+            if(temp != last_seen_index.end() && temp->second >= first) {
+                first = temp->second + 1;
+                temp->second = i;
+            }
+            // If we haven't seen this char before, hash it and store its index in s
+            else {
+                last_seen_index[s[i]] = i;
+            }
+            
+            // See if the substring we are currently computing has overtaken the previous longest substring
+            if((i - first + 1) > longest) {
+                longest = i - first + 1;
+            }
+        }
+
+        return longest;
     }
 };
